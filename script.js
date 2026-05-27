aba90f993754fb1c7a5da1b75c2c7d7a19264cad
VoidMan <120510676+VoidMan1924@users.noreply.github.com>
2026-05-27 18:40:21 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let APIKEY = "RqvdCxrTevb01yBQAPxAgkIcJAx8Mp6E";
+let offsets = {};
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+	document.getElementById("btnSearch").addEventListener("click", ev => {
+		ev.preventDefault(); //to stop the page reload
+		let str = document.getElementById("search").value.trim();
+		if (str in offsets) {
+			offsets[str] = offsets[str] + 5;
+		}
+		else{
+			offsets[str] = 0;
+		}
+		let offset = offsets[str]
+		let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=5&offset=${offset}&q=`;
+		url = url.concat(str);
+		console.log(url);
+		fetch(url)
+			.then(response => response.json())
+			.then(content => {
+			//  data, pagination, meta
+			console.log(content.data);
+			console.log("META", content.meta);
+				for (let i = 0; i < 5; i++) {
+					let fig = document.createElement('figure');
+					let img = document.createElement('img');
+					let fc = document.createElement('figcaption');
+					img.src = content.data[i].images.downsized.url;
+					img.alt = content.data[i].title;
+					fc.textContent = content.data[i].title;
+					fig.appendChild(img);
+					fig.appendChild(fc);
+					let out = document.querySelector(".out");
+					out.insertAdjacentElement('afterbegin', fig);
+				}
+				document.querySelector("#search").value = "";
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	});
+}
